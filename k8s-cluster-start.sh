d24ee49fb74a90e469a3f623ce505daa07c2d622
Sean <sean.snow@live.com>
2018-10-25 14:40:49 +0800

    init

---
+++ k8s-cluster-start.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo $HOSTNAME
+
+NAME=$NAME
+
+if [ -z "$NAME" ]; then
+  NAME=eureka-server
+fi
+
+NAMESPACE=$NAMESPACE
+
+if [ -z "$NAMESPACE" ]; then
+  NAMESPACE=default
+fi
+
+REPLICAS=$REPLICAS
+
+if [ -z "$REPLICAS" ]; then
+  REPLICAS=3
+fi
+
+ZONE=""
+
+for((i=0;i<REPLICAS;i++));
+do
+    ZONE="${ZONE}http://${NAME}-${i}.${NAME}:1818/eureka/,"
+done
+
+ZONE=${ZONE%?}
+
+export RUN_ARGS="$RUN_ARGS --eureka.instance.hostname=${HOSTNAME}.${NAME} --eureka.client.serviceUrl.defaultZone=$ZONE"
+
+echo RUN_ARGS=$RUN_ARGS
+
+/start.sh
